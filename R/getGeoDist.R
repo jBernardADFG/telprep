144d4771aa80549830a21aaf254835173c7e7ab3
Jordy Bernard <jwbernard@alaska.edu>
2020-04-08 14:36:19 -0800

    fixing dependencies

--- R/getGeoDist.R
+++ R/getGeoDist.R
@@ -7,13 +7,11 @@
 #' @param units The geodesic distance will be computed in terms of these units -- Defaults to km
 #' @return Returns the geodesic distance between two coordinate locations
 
-
-
 get.geo.dist = function(long1, lat1, long2, lat2, units = "km") {
   longlat1 = purrr::map2(long1, lat1, function(x,y) c(x,y))
   longlat2 = purrr::map2(long2, lat2, function(x,y) c(x,y))
   distance_list = purrr::map2(longlat1, longlat2, function(x,y) geosphere::distHaversine(x, y))
-  distance_m = list.extract(distance_list, position = 1)
+  distance_m = rlist::list.extract(distance_list, position = 1)
   if (units == "km") {
     distance = distance_m / 1000.0;
   }
