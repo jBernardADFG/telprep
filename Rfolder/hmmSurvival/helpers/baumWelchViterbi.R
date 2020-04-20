7ab0214cde43fa95f4a93ae87af57d7c5e578ca5
Jordy Bernard <jwbernard@alaska.edu>
2020-04-19 18:42:33 -0800

    updates made to hmmSurvival folder -- hmm is now running -- remember to add constraint on first emission matrix

--- Rfolder/hmmSurvival/helpers/baumWelchViterbi.R
+++ Rfolder/hmmSurvival/helpers/baumWelchViterbi.R
@@ -1,10 +1,20 @@
-# # Pick up here tomorrow...
-#
-# bwv <- function(msm_data){
-#   q_mat <-
-#   x <- msm(obs ~ time, subject=subject, data=msm_data, qmatrix=q_mat,
-#            hmodel = list(hmmBinom(size=3, prob=0.2),
-#                          hmmBinom(size=2, prob=0.2)))
-#   viterbi.msm(x)
-# }
+# Pick up here tomorrow...
+
+bwv <- function(msm_data){
+  obs <- msm_data$obs
+  time <- msm_data$time
+  subject <- msm_data$subject
+  q_mat <- rbind(c(0.9,0.1), c(0,1))
+  hmodel = list(hmmMV(hmmCat(prob=c(0.3,0.3,0.3)),
+                      hmmCat(prob=c(0.3,0.3,0.3))),
+                hmmMV(hmmCat(prob=c(0.3,0.3,0.3)),
+                      hmmCat(prob=c(0.3,0.3,0.3))))
+  fit_mod <- msm(obs ~ time, subject=subject, qmatrix=q_mat,
+           hmodel = hmodel, method="BFGS", control=list(maxit=5000))
+  return(fit_mod)
+}
+
+fit_mod <- bwv(msm_data)
+viterbi.msm(fit_mod)
+
 
