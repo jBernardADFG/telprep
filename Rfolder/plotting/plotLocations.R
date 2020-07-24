47069ae3d3ea8d9d82711e7aa230b6c16bc7f25c
Bernard <jordy.bernard@dfg.alaska.local>
2020-07-24 09:37:02 -0800

    open_maps=F

--- Rfolder/plotting/plotLocations.R
+++ Rfolder/plotting/plotLocations.R
@@ -3,7 +3,8 @@
 #' @param sldf Shapefile or SpatialLinesDataFrame representation of the river network.
 #' @param detects Output of \code{\link{combine_data}}, \code{\link{rm_land_detects}}, \code{\link{get_best_locations}}, \code{\link{flag_dead_fish}}, \code{\link{flag_dead_fish}}, or \code{\link{hmm_survival}}.
 #' @param extent A vector of length four specifying the plotting extent c(x_min, x_max, y_min, y_max)
-#' @param type The background to use (see \code{\link[OpenStreetMap]{openmap}}) for more information.
+#' @param open_maps If open_maps=T, a background will be plotted using (\code{\link[OpenStreetMap]{openmap}}). If open_maps=F, a black background will be used.
+#' @param type The background to use (see \code{\link[OpenStreetMap]{openmap}}) for more information. Argument is ignored if open_maps=F.
 #' @param darken Increase to darken the background when open_maps=T. Defaults to 1.
 #' @param col_by_fish col_by_fish=T assigns each fish a unique color. This color will be preserved between mappings (i.e. between different flight periods).
 #' @param flight_num Numerical argument specifying the flight period to plot. Defaults to all.
@@ -24,6 +25,9 @@
 #'
 #' # change style of background
 #' make_plot(sldf, best_detects, type="esri-topo")
+#'
+#' # remove background -- can be useful if OpenStreetMap is unavailable on your machine
+#' make_plot(sldf, best_detects, open_maps=F)
 #'
 #' # give each fish a unique color preserved through flights -- unfortunately there are only so many colors
 #' par(mfrow=c(3,1))
@@ -47,8 +51,7 @@
 #' make_plot(sldf, viterbi, type="bing", darken=2.5, viterbi=T, flight=5)
 
 
-make_plot <- function(sldf, detects, extent=NA, type="bing", darken=1, col_by_fish=F, flight_num=NA, channel=NA, tag_id=NA, viterbi=F, return_background=F){
-  open_maps=T
+make_plot <- function(sldf, detects, extent=NA, open_maps=T, type="bing", darken=1, col_by_fish=F, flight_num=NA, channel=NA, tag_id=NA, viterbi=F, return_background=F){
   if (!requireNamespace("sp", quietly = TRUE)) {
     stop("Package \"sp\" is needed for this function to work. Please install it.",
          call. = FALSE)
@@ -92,8 +95,10 @@ make_plot <- function(sldf, detects, extent=NA, type="bing", darken=1, col_by_fi
     background$tiles[[1]]$colorData <- plotwidgets::hsl2col(hsl)
     plot(background, main="",removeMargin=F)
   }else{
-    plot(raster::crop(sldf,raster::extent(sldf)), main="")
-    rect(sldf@bbox[1,1],sldf@bbox[2,1],sldf@bbox[1,2],sldf@bbox[2,2], col = "black")
+    n_sldf <- sp::spTransform(sldf, suppressWarnings(CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs")))
+
+    plot(raster::crop(n_sldf,raster::extent(n_sldf)), main="")
+    rect(n_sldf@bbox[1,1],n_sldf@bbox[2,1],n_sldf@bbox[1,2],n_sldf@bbox[2,2], col = "black")
   }
   if (is.null(detects$FlightNum)){
     bd <- detects[is.element(detects$Channel, channel) & is.element(detects$TagID, tag_id),]
@@ -105,10 +110,11 @@ make_plot <- function(sldf, detects, extent=NA, type="bing", darken=1, col_by_fi
   bd <- bd[sp::point.in.polygon(bd$X, bd$Y, x_vec, y_vec)==1,]
   c <- data.frame(bd$X, bd$Y)
   sp <- sp::SpatialPoints(c, raster::crs(sldf))
-  new_sp <- sp::spTransform(sp, background[[1]][[1]]$projection)
+  n_crs <- suppressWarnings(CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs"))
+  new_sp <- sp::spTransform(sp, n_crs)
   bd$X <- new_sp@coords[,1]
   bd$Y <- new_sp@coords[,2]
-  sldf <- sp::spTransform(sldf, background[[1]][[1]]$projection)
+  sldf <- sp::spTransform(sldf, n_crs)
   if (col_by_fish){
     set.seed(3)
     color_mat <- matrix(NA, nrow=100, ncol=100)
@@ -167,4 +173,3 @@ make_plot <- function(sldf, detects, extent=NA, type="bing", darken=1, col_by_fi
     return(background)
   }
 }
-
